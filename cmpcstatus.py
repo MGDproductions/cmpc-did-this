27906e3cd6f559eb4921f03144ad774a65ec9aea
jmcb <joelsgp@protonmail.com>
2023-04-05 18:01:16 +0100

    Better config, fix random game, trim code

--- cmpcstatus.py
+++ cmpcstatus.py
@@ -44,7 +44,7 @@ async def on_ready():
 async def on_member_join(member):
     role = get(member.guild.roles, id=932977796492427276)
     await member.add_roles(role)
-    if data['welcome'] == "true":
+    if data['welcome']:
         print(member.name + " joined")
         channel = bot.get_channel(816406795210850356)
         strip_width, strip_height = 471, 155
@@ -96,8 +96,8 @@ async def on_message(message):
     if message.content.startswith('random game'):
         async with aiohttp.ClientSession() as session:
             async with session.get('http://store.steampowered.com/explore/random/') as r:
-                shorten = r.url
-        await message.channel.send(shorten.replace('?snr=1_239_random_', ''))
+                shorten = str(r.url).replace('?snr=1_239_random_', '')
+        await message.channel.send(shorten)
 
     if message.content.startswith('random number'):
         try:
@@ -121,7 +121,7 @@ async def on_message(message):
     if message.content.startswith("random capybara"):
         # todo move to aiohttp
         img = Image.open(requests.get("https://api.capy.lol/v1/capybara", stream=True).raw)
-        savestring = "capybara" + str(random.randint(0,100000)) + ".png"
+        savestring = "capybara" + str(random.randint(0, 100000)) + ".png"
         rgb_im = img.convert('RGB')
         rgb_im.save(savestring,"PNG")
         embed=discord.Embed(title="capybara for u!", color=0xff0000)
@@ -154,7 +154,7 @@ async def on_message(message):
     await bot.process_commands(message)
 
 async def clock():
-    if data['clock'] == "true":
+    if data['clock']:
         amsterdam = pytz.timezone('Europe/Amsterdam')
         datetime_amsterdam = datetime.datetime.now(amsterdam)
         ams_time = datetime_amsterdam.strftime("%H:%M")
@@ -168,7 +168,7 @@ async def clock():
         asyncio.ensure_future(clock())
     
 async def fish():
-    if data['fishgamingwednesday'] == "true":
+    if data['fishgamingwednesday']:
         global fishgaming
         global fishrestarting
         gmt = pytz.timezone('GMT')
@@ -211,93 +211,18 @@ async def fish():
                 await asyncio.sleep(60)
 
                 perms = channel.overwrites_for(channel.guild.default_role)
-                perms.view_channel=False
+                perms.view_channel = False
                 #not working but needs to be fixed
                 #perms.create_public_threads=False
                 #perms.create_private_threads=False
                 #perms.send_messages_in_threads=False
                 await channel.set_permissions(channel.guild.default_role, overwrite=perms)
-                embed6=discord.Embed(title="Fish gaming wednesday has ended.", color=0x69CCE7)
+                embed6 = discord.Embed(title="Fish gaming wednesday has ended.", color=0x69CCE7)
                 embed6.set_image(url=("attachment://" + "fgwends.png"))
                 await message.edit(embed=embed6)
                 fishgaming = False
         await asyncio.sleep(60)
         asyncio.ensure_future(fish())
-        
-# if data['birthday'] == "true":
-#     @tasks.loop(minutes=1)
-#     async def birthday_update():
-#             global birthday
-#             cest = pytz.timezone('Europe/London')
-#             datetime_cest = datetime.datetime.now(cest)
-#             weekday = False
-#             channel = bot.get_channel(982687737503182858)
-#             if "06-05" in str(datetime_cest):
-#                 if birthday == False:
-#                     perms = channel.overwrites_for(channel.guild.default_role)
-#                     perms.send_messages=True
-#                    perms.view_channel=True
-#                    await channel.set_permissions(channel.guild.default_role, overwrite=perms)
-#                    await channel.send("@everyone It's Marcel's birthday today! As a birthday gift he wants all the cat pictures in the world. Drop them in this chat before he wakes up!")
-#                    birthday = True
-#                    print("Marcel's birthday started")
-#       #              await channel.send(file=discord.File(r'birthday.mp4'))
-#            if "06-05" not in str(datetime_cest):
-#                if birthday == True:
-#                    perms = channel.overwrites_for(channel.guild.default_role)
-#                    perms.send_messages=False
-#                    await channel.set_permissions(channel.guild.default_role, overwrite=perms)
-#                    embed=discord.Embed(title="Marcel's birthday has ended.", color=0x69CCE7)
-#                    embed.add_field(name="In 5 minutes this channel will be hidden.", value="** **", inline=False)
-#                    message = await channel.send(embed=embed)
-#                    await asyncio.sleep(60)
-#                    embed2=discord.Embed(title="Marcel's birthday has ended.", color=0x69CCE7)
-#                    embed2.add_field(name="In 4 minutes this channel will be hidden.", value="** **", inline=False)
-#                    await message.edit(embed=embed2)
-#                    await asyncio.sleep(60)
-#                    embed3=discord.Embed(title="Marcel's birthday has ended.", color=0x69CCE7)
-#                    embed3.add_field(name="In 3 minutes this channel will be hidden.", value="** **", inline=False)
-#                    await message.edit(embed=embed3)
-#                    await asyncio.sleep(60)
-#                    embed4=discord.Embed(title="Marcel's birthday has ended.", color=0x69CCE7)
-#                    embed4.add_field(name="In 2 minutes this channel will be hidden.", value="** **", inline=False)
-#                    await message.edit(embed=embed4)
-#                    await asyncio.sleep(60)
-#            #        embed5=discord.Embed(title="Marcel's birthday has ended.", color=0x69CCE7)
-#                    embed5.add_field(name="In 1 minute this channel will be hidden.", value="** **", inline=False)
-#                    await message.edit(embed=embed5)
-#                    await asyncio.sleep(60)
-#                    perms = channel.overwrites_for(channel.guild.default_role)
-#                    perms.view_channel=False
-#                    await channel.set_permissions(channel.guild.default_role, overwrite=perms)
-#                    embed6=discord.Embed(title="Marcel's birthday has ended.", color=0x69CCE7)
-#                    await message.edit(embed=embed6)
-#                    birthday = False
-#
-#    @birthday_update.before_loop
-#    async def before_birthday_update():
-#        await bot.wait_until_ready()
-#    birthday_update.start()
-#
-# @tasks.loop(minutes=10)
-# async def countdown():
-#     difference = datetime.datetime(2021, 7, 4, 18, 0, 0) - datetime.datetime.now()
-#     count_hours, rem = divmod(difference.seconds, 3600)
-#     count_minutes, count_seconds = divmod(rem, 60)
-#     channel = bot.get_channel(858737401048465420)
-#     channel2 = bot.get_channel(858737233112727552)
-#     if difference.days < 0:
-#         await channel.delete()
-#         await channel2.edit(name="Goodbye cmpc.")
-#         countdown.cancel()
-#     countdowntime = (str(count_hours) + "h " + str(count_minutes) + "m ")
-#     print(countdowntime)
-#     await channel.edit(name=countdowntime)
-#
-# @countdown.before_loop
-# async def before_countdown():
-#     await bot.wait_until_ready()
-#     countdown.start()
 
 
 def main():
