d026290e62381db3ce02a0273b95b785055d1287
jmcb <joelsgp@protonmail.com>
2023-06-01 19:22:33 +0100

    Move constants

--- cmpcstatus/__init__.py
+++ cmpcstatus/__init__.py
@@ -12,7 +12,6 @@ import urllib.parse
 from io import BytesIO
 from tempfile import TemporaryFile
 from typing import Literal, Optional
-from zoneinfo import ZoneInfo
 
 import aiohttp
 import aiosqlite
@@ -25,79 +24,52 @@ from PIL import Image, ImageDraw, ImageFont
 
 from assets.words import common_words
 
-
-# CONSTANTS
-INTENTS = discord.Intents.default()
-INTENTS.members = True
-INTENTS.message_content = True
-
-ENABLE_BIRTHDAY = True
-ENABLE_CLOCK = True
-ENABLE_FISH = True
-ENABLE_PROFANITY = True
-ENABLE_READY_MESSAGE = True
-ENABLE_WELCOME = True
-
-PATH_CONFIG = "config.toml"
-PATH_DATABASE = "db.sqlite3"
-
-GUILD_EGGYBOI = 714154158969716780
-ROLE_DEVELOPER = 741317598452645949
-ROLE_FISH = 875359516131209256
-ROLE_MEMBER = 932977796492427276
-ROLE_MODS = 725356663850270821
-
-TEXT_CHANNEL_BIRTHDAY = 982687737503182858
-TEXT_CHANNEL_FISH = 875297517351358474
-TEXT_CHANNEL_GENERAL = 714154159590473801
-TEXT_CHANNEL_BOT_COMMANDS = 736664393630220289
-VOICE_CHANNEL_CLOCK = 753467367966638100
-
-CHANNEL_PERMISSIONS_OPEN = {"view_channel": True, "send_messages": True}
-CHANNEL_PERMISSIONS_LOCKED = {"view_channel": True, "send_messages": False}
-CHANNEL_PERMISSIONS_HIDDEN = {"view_channel": False, "send_messages": False}
-
-COLOUR_GREEN = discord.Color.green()
-COLOUR_RED = discord.Color.red()
-COLOUR_BLUE = discord.Color.blue()
-
-EMOJI_BIBI_PARTY = discord.PartialEmoji.from_str("<:bibi_party:857659475687374898>")
-EMOJI_SAT_CAT = discord.PartialEmoji.from_str("<:sad_cat:770191103310823426>")
-EMOJI_SKULL = discord.PartialEmoji.from_str("💀")
-
-MENTION_NONE = discord.AllowedMentions.none()
-
-TZ_AMSTERDAM = ZoneInfo("Europe/Amsterdam")
-TZ_LONDON = ZoneInfo("Europe/London")
-ISO_WEEKDAY_WEDNESDAY = 3
-ISO_WEEKDAY_THURSDAY = 4
-CLOCK_TIMES = [
-    datetime.time(hour=h, minute=m, tzinfo=TZ_AMSTERDAM)
-    for m in range(0, 60, 10)
-    for h in range(24)
-]
-TIME_MIDNIGHT = datetime.time(hour=0, tzinfo=TZ_AMSTERDAM)
-TIME_FGW_START = TIME_MIDNIGHT
-TIME_FGW_LOCK = TIME_MIDNIGHT
-TIME_FGW_END = datetime.time(hour=0, minute=5, tzinfo=TZ_AMSTERDAM)
-TIME_BIRTHDAY_START = TIME_MIDNIGHT
-TIME_BIRTHDAY_END = TIME_MIDNIGHT
-DATE_BIRTHDAY_MONTH = 6
-DATE_BIRTHDAY_DAY = 5
-# how many seconds in a minute
-COUNTDOWN_MINUTE = 60
-
-PROFANITY_INTERCEPT = (":3",)
-PROFANITY_ROWS_DEFAULT = 5
-PROFANITY_ROWS_MAX = 100
-PROFANITY_ROWS_INLINE = False
-
-COMMAND_PREFIX = [
-    "random ",  # space is needed
-    "cmpc.",
-    "c.",
-    "$",
-]
+from constants import (
+    CHANNEL_PERMISSIONS_HIDDEN,
+    CHANNEL_PERMISSIONS_LOCKED,
+    CHANNEL_PERMISSIONS_OPEN,
+    CLOCK_TIMES,
+    COLOUR_BLUE,
+    COLOUR_GREEN,
+    COLOUR_RED,
+    COMMAND_PREFIX,
+    COUNTDOWN_MINUTE,
+    DATE_BIRTHDAY_DAY,
+    DATE_BIRTHDAY_MONTH,
+    EMOJI_BIBI_PARTY,
+    EMOJI_SAT_CAT,
+    EMOJI_SKULL,
+    ENABLE_BIRTHDAY,
+    ENABLE_CLOCK,
+    ENABLE_FISH,
+    ENABLE_PROFANITY,
+    ENABLE_READY_MESSAGE,
+    ENABLE_WELCOME,
+    GUILD_EGGYBOI,
+    INTENTS,
+    ISO_WEEKDAY_THURSDAY,
+    ISO_WEEKDAY_WEDNESDAY,
+    MENTION_NONE,
+    PATH_CONFIG,
+    PATH_DATABASE,
+    PROFANITY_INTERCEPT,
+    PROFANITY_ROWS_DEFAULT,
+    PROFANITY_ROWS_INLINE,
+    PROFANITY_ROWS_MAX,
+    ROLE_DEVELOPER,
+    ROLE_FISH,
+    ROLE_MEMBER,
+    TEXT_CHANNEL_BIRTHDAY,
+    TEXT_CHANNEL_BOT_COMMANDS,
+    TEXT_CHANNEL_FISH,
+    TEXT_CHANNEL_GENERAL,
+    TIME_BIRTHDAY_START,
+    TIME_FGW_END,
+    TIME_FGW_LOCK,
+    TIME_FGW_START,
+    TZ_AMSTERDAM,
+    VOICE_CHANNEL_CLOCK,
+)
 
 
 log = logging.getLogger(__name__)
@@ -384,7 +356,7 @@ class Birthday(BotCog):
         await channel.send(
             f"{EMOJI_BIBI_PARTY}{EMOJI_BIBI_PARTY}{EMOJI_BIBI_PARTY} @everyone It's Marcel's birthday today!"
             " As a birthday gift he wants all the cat pictures in the world."
-            F" Drop them in this chat before he wakes up!{EMOJI_BIBI_PARTY}{EMOJI_BIBI_PARTY}{EMOJI_BIBI_PARTY}",
+            f" Drop them in this chat before he wakes up!{EMOJI_BIBI_PARTY}{EMOJI_BIBI_PARTY}{EMOJI_BIBI_PARTY}",
             file=file,
         )
 
@@ -737,7 +709,9 @@ class BasicCommands(BotCog):
         if upload:
             async with ctx.typing():
                 with TemporaryFile() as file:
-                    subprocess.run(["git", "archive", "--format=zip", "HEAD"], stdout=file)
+                    subprocess.run(
+                        ["git", "archive", "--format=zip", "HEAD"], stdout=file
+                    )
                     file.seek(0)
                     discord_file = discord.File(file, filename="source.zip")
                     await message.reply(file=discord_file)
---
+++ cmpcstatus/constants.py
@@ -0,0 +1,87 @@
+import datetime
+from zoneinfo import ZoneInfo
+
+import discord
+
+
+# bot API intents
+INTENTS = discord.Intents.default()
+INTENTS.members = True
+INTENTS.message_content = True
+
+# bot features
+ENABLE_BIRTHDAY = True
+ENABLE_CLOCK = True
+ENABLE_FISH = True
+ENABLE_PROFANITY = True
+ENABLE_READY_MESSAGE = True
+ENABLE_WELCOME = True
+
+# file locations
+PATH_CONFIG = "config.toml"
+PATH_DATABASE = "db.sqlite3"
+
+# discord guild, role, and channel IDs
+GUILD_EGGYBOI = 714154158969716780
+ROLE_DEVELOPER = 741317598452645949
+ROLE_FISH = 875359516131209256
+ROLE_MEMBER = 932977796492427276
+ROLE_MODS = 725356663850270821
+
+TEXT_CHANNEL_BIRTHDAY = 982687737503182858
+TEXT_CHANNEL_FISH = 875297517351358474
+TEXT_CHANNEL_GENERAL = 714154159590473801
+TEXT_CHANNEL_BOT_COMMANDS = 736664393630220289
+VOICE_CHANNEL_CLOCK = 753467367966638100
+
+# discord channel permissions
+CHANNEL_PERMISSIONS_OPEN = {"view_channel": True, "send_messages": True}
+CHANNEL_PERMISSIONS_LOCKED = {"view_channel": True, "send_messages": False}
+CHANNEL_PERMISSIONS_HIDDEN = {"view_channel": False, "send_messages": False}
+
+# discord colour objects
+COLOUR_GREEN = discord.Color.green()
+COLOUR_RED = discord.Color.red()
+COLOUR_BLUE = discord.Color.blue()
+
+# discord emote objects
+EMOJI_BIBI_PARTY = discord.PartialEmoji.from_str("<:bibi_party:857659475687374898>")
+EMOJI_SAT_CAT = discord.PartialEmoji.from_str("<:sad_cat:770191103310823426>")
+EMOJI_SKULL = discord.PartialEmoji.from_str("💀")
+
+MENTION_NONE = discord.AllowedMentions.none()
+
+# date and time information for events
+TZ_AMSTERDAM = ZoneInfo("Europe/Amsterdam")
+TZ_LONDON = ZoneInfo("Europe/London")
+ISO_WEEKDAY_WEDNESDAY = 3
+ISO_WEEKDAY_THURSDAY = 4
+CLOCK_TIMES = [
+    datetime.time(hour=h, minute=m, tzinfo=TZ_AMSTERDAM)
+    for m in range(0, 60, 10)
+    for h in range(24)
+]
+TIME_MIDNIGHT = datetime.time(hour=0, tzinfo=TZ_AMSTERDAM)
+TIME_FGW_START = TIME_MIDNIGHT
+TIME_FGW_LOCK = TIME_MIDNIGHT
+TIME_FGW_END = datetime.time(hour=0, minute=5, tzinfo=TZ_AMSTERDAM)
+TIME_BIRTHDAY_START = TIME_MIDNIGHT
+TIME_BIRTHDAY_END = TIME_MIDNIGHT
+DATE_BIRTHDAY_MONTH = 6
+DATE_BIRTHDAY_DAY = 5
+# how many seconds in a minute
+COUNTDOWN_MINUTE = 60
+
+# profanity config
+PROFANITY_INTERCEPT = (":3",)
+PROFANITY_ROWS_DEFAULT = 5
+PROFANITY_ROWS_MAX = 100
+PROFANITY_ROWS_INLINE = False
+
+# bot command prefices
+COMMAND_PREFIX = [
+    "random ",  # space is needed
+    "cmpc.",
+    "c.",
+    "$",
+]
